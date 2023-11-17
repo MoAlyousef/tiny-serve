f4425716e52adf42a145085abd5f42cf587bce84
MoAlyousef <may642_2000@hotmail.com>
2023-11-17 23:38:43 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: tiny-serve PORT DIR");
-        return;
+struct Opts<'a> {
+    port: u16,
+    root: &'a str,
+}
+
+impl<'a> Default for Opts<'a> {
+    fn default() -> Self {
+        Self {
+            port: 8000,
+            root: ".",
+        }
     }
-    let mut server = livid_server::Server::new(args[1].parse().unwrap());
-    server.serve_dir(&args[2]);
+}
+
+impl<'a> Opts<'a> {
+    pub fn new(args: &'a [String]) -> Self {
+        let port = args[0].parse().unwrap_or(8000);
+        let root = args[1].as_str();
+        Self {
+            port, root,
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let opts = if args.is_empty() {
+        Opts::default()
+    } else {
+        Opts::new(&args)
+    };
+    eprintln!("Serving HTTP on 127.0.0.1 port {0} (http://127.0.0.1:{0}/) ...", opts.port);
+    let mut server = livid_server::Server::new(opts.port);
+    server.serve_dir(&opts.root.to_string());
+    server.log_output(Box::new(std::io::stdout()));
     server.serve();
 }
